3c4c7f7e7a49fa1357dd6190b96174c3548741ce
Kevin Montanez <kevindanilo281@gmail.com>
2025-08-27 18:02:32 -0500

    funcionalidad de js teniendo en cuenta los porcentajes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,60 @@
 function simulador(){
-    alert("hola negro")
+    
 
-    // let nombres = document.getElementById('nombres');
-    // let telefono = document.getElementById('telefono');
-    // let email = document.getElementById('email');
-    // let monto = document.getElementById('monto');
-    // let tiempo = document.getElementById('tiempo');
+    // VALORES DE LOS CAMPOS DEL FORMULARIO
+    let nombres = document.getElementById('nombres').value
+    let telefono = document.getElementById('telefono').value
+    let email = document.getElementById('email').value
+    let monto = document.getElementById('monto').value
+    let tiempo = document.getElementById('tiempo').value
 
+    // CONSTANTES QUE CAPTURAN LAS ETIQUETAS SPAN EN LAS QUE SE MUESTRAN LOS VALORES 
+
+
+    const showTiempo = document.getElementById('showTiempo')
+    const showInteres = document.getElementById('showInteres')
+    const showTotal= document.getElementById('showTotal')
+    const showGanancia = document.getElementById('showGanancia')
+    const showNombres = document.getElementById('showNombres')
+    const showCorreo = document.getElementById('showCorreo')
+
+    // variables para realizar las operaciones 
+
+    let ganancia = 0
+    let total = 0
+
+
+    switch (tiempo) {
+        // en este case realizaremos el calculo si el tiempo corresponde a 1 años
+        case '1':
+            ganancia = (monto*0.008)*12
+            total = parseInt (monto) + parseInt(ganancia)
+            showInteres.textContent = '0.8%'
+            showTiempo.textContent = '12 MESES'
+            break;
+
+        // en este case realizaremos el calculo si el tiempo corresponde a 2 años
+        case '2':
+            ganancia = (monto*0.013)*24
+            total = parseInt(monto) + parseInt(ganancia)
+            showInteres.textContent = '0.13%'
+            showTiempo.textContent = '24 MESES'
+            break;
+
+        // en este case realizaremos el calculo si el tiempo corresponde a 3 años
+        case '3':
+            ganancia = (monto * 0.017)*36
+            total = parseInt(monto) + parseInt(ganancia)
+            showInteres.textContent = '1.17%'
+            showTiempo.textContent = '36 MESES'
+        break;
+    
+        default:
+            break;
+    }
+
+    showTotal.textContent = total
+    showGanancia.textContent = ganancia
 
 
     const contpre = document.getElementById('pre-simulation')
